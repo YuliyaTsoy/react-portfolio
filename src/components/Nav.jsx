ce22fa1bf02fa50e102bac2fcd4bbd8001f273c3
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-05 17:35:50 -0500

    more css to Nav jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,17 @@
-
+import {CgNametag} from 'react-icons/cg'
 
 const nav = ()=> {
     return (
         <>
-        <div className="flex items-center justify-between p-10">
-            <div>LOGO</div>
+        <div className="flex items-center justify-between p-10 lg:flex-row">
             <div>
-                <a href="#">About</a>
-                <a href="#">Portfolio</a>
-                <a href="#">Contact</a>
-                <a href="#">Resume</a>
+                <a href="#" className = "text-white font-mono text-3xl tracking-wider flex items-center"><CgNametag/>LOGO</a>
+            </div>
+            <div className="space-x-4">
+                <a href="#" className="text-white hover:bg-blue-800 rounded-full px-5 py-2 text-xl">About</a>
+                <a href="#" className="text-white hover:bg-blue-800 rounded-full px-5 py-2 text-xl">Portfolio</a>
+                <a href="#" className="text-white hover:bg-blue-800 rounded-full px-5 py-2 text-xl">Contact</a>
+                <a href="#" className="text-white hover:bg-blue-800 rounded-full px-5 py-2 text-xl">Resume</a>
             </div>
         </div>
         </>
